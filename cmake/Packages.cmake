74d9bdaecbc29b570be1819878773e80a1caa376
Jeerabhat Supapinit <kaopancraft@gmail.com>
2026-03-17 15:19:16 +0700

    feat: add assimp

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,5 +1,6 @@
-Set(FETCHCONTENT_QUIET FALSE)
+set(FETCHCONTENT_QUIET FALSE)
 
+# --- GLFW ---
 CPMAddPackage(
   NAME glfw
   VERSION 3.3.10
@@ -10,44 +11,69 @@ CPMAddPackage(
           "GLFW_BUILD_TESTS OFF"
           "GLFW_BUILD_DOCS OFF"
 )
-find_targets(glfw_targets ${glfw_SOURCE_DIR})
-make_folder("glfw" ${glfw_targets})
+if(glfw_ADDED)
+  find_targets(glfw_targets ${glfw_SOURCE_DIR})
+  make_folder("glfw" ${glfw_targets})
+endif()
 
+# --- GLAD ---
 CPMAddPackage(
   NAME glad
   GITHUB_REPOSITORY Dav1dde/glad
   VERSION 2.0.6
   DOWNLOAD_ONLY
 )
-set(CMAKE_MODULE_PATH
-  ${CMAKE_MODULE_PATH}
-  ${glad_SOURCE_DIR}/cmake)
-include(GladConfig)
-add_subdirectory("${glad_SOURCE_DIR}/cmake" glad_cmake)
-set(GLAD_LIBRARY glad_gl_core_46)
-# https://github.com/Dav1dde/glad/wiki/C#generating-during-build-process
-glad_add_library(${GLAD_LIBRARY} SHARED API gl:core=4.6)
-make_folder("glad" ${GLAD_LIBRARY})
-install(TARGETS ${GLAD_LIBRARY}
-  EXPORT ${GLAD_LIBRARY}-targets
-  RUNTIME DESTINATION bin
-  ARCHIVE DESTINATION lib
-  LIBRARY DESTINATION lib)
+if(glad_ADDED)
+  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${glad_SOURCE_DIR}/cmake)
+  include(GladConfig)
+  add_subdirectory("${glad_SOURCE_DIR}/cmake" glad_cmake)
+  set(GLAD_LIBRARY glad_gl_core_46)
+  # https://github.com/Dav1dde/glad/wiki/C#generating-during-build-process
+  glad_add_library(${GLAD_LIBRARY} SHARED API gl:core=4.6)
+  make_folder("glad" ${GLAD_LIBRARY})
+  install(TARGETS ${GLAD_LIBRARY}
+    EXPORT ${GLAD_LIBRARY}-targets
+    RUNTIME DESTINATION bin
+    ARCHIVE DESTINATION lib
+    LIBRARY DESTINATION lib)
+endif()
 
-
+# --- GLM ---
 CPMAddPackage(
   NAME glm
   GITHUB_REPOSITORY g-truc/glm
   GIT_TAG 0.9.9.8
 )
-
 if(glm_ADDED)
   make_folder("glm" glm)
 endif()
 
 
+
+# --- Assimp ---
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(MINIZIP REQUIRED minizip)
+
+include_directories(${MINIZIP_INCLUDE_DIRS})
+
+CPMAddPackage(
+  NAME assimp
+  GITHUB_REPOSITORY assimp/assimp
+  VERSION 5.4.3
+  OPTIONS "ASSIMP_BUILD_TESTS OFF"
+          "ASSIMP_BUILD_SAMPLES OFF"
+          "ASSIMP_BUILD_ASSIMP_TOOLS OFF"
+          "ASSIMP_INSTALL_PDB OFF"
+          "BUILD_SHARED_LIBS ON"
+)
+if(assimp_ADDED)
+  make_folder("assimp" assimp)
+endif()
+
+# --- Utilities ---
 # To fix build errors in Qt Creator for the latest versions of CMake.
 set(CMP0169 OLD)
+
 # Source file grouping of visual studio and xcode
 CPMAddPackage(
   NAME GroupSourcesByFolder.cmake
